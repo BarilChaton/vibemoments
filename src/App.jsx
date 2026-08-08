b97342981752835a3b590554fa9459c2172d4267
Karl Christian Karlsson Korbacz <kodman1991@hotmail.com>
2026-08-08 10:05:40 +0200

    Initial VibeMoments project setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main className="flex h-dvh items-center justify-center bg-black text-white">
+      <h1 className="text-3xl font-bold">VibeMoments</h1>
+    </main>
+  )
+}
+
+export default App
